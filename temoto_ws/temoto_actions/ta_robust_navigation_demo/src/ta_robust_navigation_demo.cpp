1173d217f1e2e59555d290a06a7455d3b96c496b
Robert <valnerrobert@gmail.com>
2021-05-29 22:17:02 +0300

    WIP yaml bugfixes

--- temoto_ws/temoto_actions/ta_robust_navigation_demo/src/ta_robust_navigation_demo.cpp
+++ temoto_ws/temoto_actions/ta_robust_navigation_demo/src/ta_robust_navigation_demo.cpp
@@ -49,57 +49,62 @@ void executeTemotoAction()
 
   std::string robot_name = "jackal";
 
-  /*
-   * Load the robot
-   */
-  TEMOTO_INFO_STREAM("loading " << robot_name);
-  rmi_.loadRobot(robot_name);
-  TEMOTO_INFO_STREAM(robot_name << " initialized");
+  // /*
+  //  * Load the robot
+  //  */
+  // TEMOTO_INFO_STREAM("loading " << robot_name);
+  // rmi_.loadRobot(robot_name);
+  // TEMOTO_INFO_STREAM(robot_name << " initialized");
 
   TEMOTO_INFO_STREAM("trying to get config of '" << robot_name << "' ...");
   YAML::Node robot_config = rmi_.getRobotConfig(robot_name);
   TEMOTO_INFO_STREAM("Config of robot '" << robot_name << "': " << robot_config);
 
+  TEMOTO_WARN_STREAM(robot_config["description"].as<std::string>());
   std::string robot_absolute_namespace = robot_config["robot_absolute_namespace"].as<std::string>();
-  std::string robot_scan_topic_from = robot_absolute_namespace + "/" + "front/scan";
-  std::string robot_scan_topic_to = robot_absolute_namespace + "/"
+  std::string lidar_scan_topic = robot_absolute_namespace + "/"
     + robot_config["navigation"]["controller"]["scan_topic"].as<std::string>();
-  TEMOTO_INFO_STREAM("robot '" << robot_name << "' expects lidar scans at topic '" << robot_scan_topic_to << "'");
+  TEMOTO_INFO_STREAM("robot '" << robot_name << "' expects lidar scans at topic '" << lidar_scan_topic << "'");
 
   /*
-   * Load the 2D lidar for the robot
+   * Load a 2D lidar for teleoperation feedback
    */
-  load_component_query_.request.component_type = "2d_lidar";
-  load_component_query_.request.additional_args = robot_scan_topic_from + " " + robot_scan_topic_to;
-  cmi_.startComponent(load_component_query_);
+  ComponentTopicsReq requested_topics;
+  temoto_component_manager::LoadComponent load_component_query;
+  std::string sensor_type = "2d_lidar";
 
-  /*
-   * Move the robot
-   */
-  geometry_msgs::PoseStamped target_pose;
-  target_pose.pose.position.x = -7;
-  target_pose.pose.position.y = 4;
-  target_pose.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0, 0, 0);;
+  TEMOTO_INFO_STREAM("Starting the " << sensor_type << " component ...");
+  requested_topics.addOutputTopic("lidar_data_2d", lidar_scan_topic);
+  ComponentTopicsRes responded_topics = cmi_.startComponent(sensor_type, requested_topics);
 
-  bool goal_reached = false;
-  while (!goal_reached)
-  try
-  {
-    TEMOTO_INFO_STREAM("Sending a navigation goal to " << robot_name << " ...");
-    if (rmi_.navigationGoal(robot_name, "map", target_pose))
-    {
-      TEMOTO_INFO_STREAM("Done navigating");
-      goal_reached = true;
-    }
-    else
-    {
-      TEMOTO_INFO_STREAM("The goal was not reached, requesting the same navigation goal again ... ");
-    }
-  }
-  catch(const resource_registrar::TemotoErrorStack &e)
-  {
-    TEMOTO_WARN_STREAM("Caught an error, requesting the same navigation goal again ... ");
-  }
+  ros::Duration(10).sleep();
+  // /*
+  //  * Move the robot
+  //  */
+  // geometry_msgs::PoseStamped target_pose;
+  // target_pose.pose.position.x = -7;
+  // target_pose.pose.position.y = 4;
+  // target_pose.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0, 0, 0);;
+
+  // bool goal_reached = false;
+  // while (!goal_reached)
+  // try
+  // {
+  //   TEMOTO_INFO_STREAM("Sending a navigation goal to " << robot_name << " ...");
+  //   if (rmi_.navigationGoal(robot_name, "map", target_pose))
+  //   {
+  //     TEMOTO_INFO_STREAM("Done navigating");
+  //     goal_reached = true;
+  //   }
+  //   else
+  //   {
+  //     TEMOTO_INFO_STREAM("The goal was not reached, requesting the same navigation goal again ... ");
+  //   }
+  // }
+  // catch(const resource_registrar::TemotoErrorStack &e)
+  // {
+  //   TEMOTO_WARN_STREAM("Caught an error, requesting the same navigation goal again ... ");
+  // }
 }
 
 // Destructor
